315cdaf872b427af27e9f9606a6a1624bb907a52
chriscoates <coates.c@hotmail.com>
2017-11-07 20:27:56 +0000

    adding CSHomeworkContextFactory

---
+++ CSHomeworkAPI/CSHomeworkContextFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using CSHomeworkAPI.Entities;
+
+namespace CSHomeworkAPI
+{
+    public class CSHomeworkContextFactory : IDesignTimeDbContextFactory<CSHomeworkContext>
+    {
+        private readonly IConfiguration _configuration;
+
+        public CSHomeworkContextFactory(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public CSHomeworkContext CreateDbContext(string[] args)
+        {
+            var connectionString = _configuration["connectionStrings:DefaultConnection"];
+
+            var optionsBuilder = new DbContextOptionsBuilder<CSHomeworkContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new CSHomeworkContext(optionsBuilder.Options);
+        }
+    }
+}
