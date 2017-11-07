e89b8935772c9f837f47023c06bb47a68112d7d6
chriscoates <coates.c@hotmail.com>
2017-11-07 20:14:40 +0000

    adding DBContext

---
+++ CSHomeworkAPI/Entities/CSHomeworkContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSHomeworkAPI.Entities
+{
+    public class CSHomeworkContext : DbContext
+    {
+        public CSHomeworkContext(DbContextOptions<CSHomeworkContext> options)
+                : base(options)
+        {
+        }
+
+        public DbSet<Question> Question { get; set; }
+    }
+
+}
