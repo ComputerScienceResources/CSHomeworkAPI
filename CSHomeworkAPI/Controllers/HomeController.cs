d5d3b1d2f8a80022b92f17761443e260a24ab315
chriscoates <coates.c@hotmail.com>
2017-11-07 21:00:25 +0000

    update namespace

--- CSHomeworkAPI/Controllers/HomeController.cs
+++ CSHomeworkAPI/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace CityInfo.API.Controllers
+namespace CSHomeworkAPI.Controllers
 {
     public class HomeController : Controller
     {
