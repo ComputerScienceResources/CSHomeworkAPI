b64cf29474a260545120474b841da6119c849c1f
chriscoates <coates.c@hotmail.com>
2017-11-07 20:07:27 +0000

    adding question entity

---
+++ CSHomeworkAPI/Entities/Question.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSHomeworkAPI.Entities
+{
+    public class Question
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Text { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public string ClassName { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public string Type { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string PlaceHolder { get; set; }
+
+        [MaxLength(255)]
+        public string Pattern { get; set; }
+
+        public int Min { get; set; }
+
+        public int Max { get; set; }
+
+        public int MinLength { get; set; }
+
+        public int MaxLength { get; set; }
+
+        public string Rows { get; set; }
+
+        public string Cols { get; set; }
+
+        [MaxLength(255)]
+        public bool CorrectAnswer { get; set; }
+
+        [MaxLength(255)]
+        public bool Explanation { get; set; }
+    }
+}
